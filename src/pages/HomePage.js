9dcfd05288ca15dd5aefe2f0ade7f40137d785fc
TheBombTNT <42681446+TheBombTNT@users.noreply.github.com>
2020-08-24 17:16:33 -0400

    Test Change

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class HomePage extends Component {
     render() {
         return (
-            <div>
-               <h1>Home</h1>
+            <div className="card text-center">
+                <h1>Home</h1>
+                <div className="overflow">
+                    <img src="" alt="" />
+                </div>
             </div>
         );
     }
