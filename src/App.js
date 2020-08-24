de45ffe8e1f4b4a4f3e997d9e69d30c60b46e479
Darreal Chambers <dchambers@Darreals-MacBook-Pro.local>
2020-08-23 23:03:57 -0400

    navbar created and login link added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 'home'
+    }
+
+    this.handleNavClick = this.handleNavClick.bind(this);
+  }
+
+  changePage(page) {
+    switch(page) {
+      case 'home':
+        return <HomePage/>;
+      case 'login':
+        return <Login/>;
+      default:
+        return <HomePage/>;
+    }
+  }
+
+  handleNavClick(event, newPage) {
+    this.setState({
+      page: newPage
+    });
+
+    // prevent reload of page caused by clicking links
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a href="#" className="navbar-brand" onClick={(event) => this.handleNavClick(event, 'home')}>Site Name</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a href="#" className="nav-link" onClick={(event) => this.handleNavClick(event, 'home')}>Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link" onClick={(event) => this.handleNavClick(event, 'login')}>Login</a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
+            <button className="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+
+      {this.changePage(this.state.page)}
+
+      </div>
+      
+    );
+  }
 }
 
 export default App;
