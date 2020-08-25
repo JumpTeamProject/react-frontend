b82e298d17689dd0dbfcf7300942f8cb41ac165b
crives <car.rives@gmail.com>
2020-08-24 23:54:35 -0400

    Update

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div className="card text-center">
+                <h1>Home</h1>
+                <div className="overflow">
+                    <img src="" alt="" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
