bdec044ab4fb5a39b063e4fd19c6c86ee93884e2
Thien Nguyen <65026941+Thien-Nguyen-Git@users.noreply.github.com>
2020-08-25 13:52:57 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
@@ -14,13 +15,13 @@ class App extends Component {
   }
 
   changePage(page) {
-    switch(page) {
+    switch (page) {
       case 'home':
-        return <HomePage/>;
+        return <HomePage />;
       case 'login':
-        return <Login/>;
+        return <Login />;
       default:
-        return <HomePage/>;
+        return <HomePage />;
     }
   }
 
@@ -36,31 +37,31 @@ class App extends Component {
   render() {
     return (
       <div>
-      <nav className="navbar navbar-expand-lg nav fadeIn third">
-        <a href="#" className="navbar-brand" onClick={(event) => this.handleNavClick(event, 'home')}>Chowzers</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a href="#" className="nav-link" onClick={(event) => this.handleNavClick(event, 'home')}>Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link" onClick={(event) => this.handleNavClick(event, 'login')}>Login</a>
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
-            <button className="btn btn-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
+        <nav className="navbar navbar-expand-lg nav fadeIn third">
+          <a href="#" className="navbar-brand title" onClick={(event) => this.handleNavClick(event, 'home')}>Chowzers</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a href="#" className="nav-link" onClick={(event) => this.handleNavClick(event, 'home')}>Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link" onClick={(event) => this.handleNavClick(event, 'login')}>Login</a>
+              </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <input className="form-control mr-sm-2" type="text" placeholder="Search" />
+              <button className="btn btn-danger my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
 
-      {this.changePage(this.state.page)}
+        {this.changePage(this.state.page)}
 
       </div>
-      
+
     );
   }
 }
