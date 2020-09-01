53a040188f39ef9d50f0e10ac93f81b9c2b08ede
Darreal Chambers <dchambers@Darreals-MacBook-Pro.local>
2020-09-01 12:35:29 -0400

    button update/removed background

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Card from './CardUI';
+import Card from '../CardUI';
 
 import img1 from '../image/mcdonalds.jpg';
 import img2 from '../image/burgerking.jpg';
@@ -8,18 +8,8 @@ import img3 from '../image/wendys.jpg';
 class HomePage extends Component {
     render() {
         return (
-            <div className="container-fluid d-flex justify-content-center">
-                <div className="row">
-                    <div className="col-md-4">
-                        <Card imgsrc={img1} title="Mcdonalds" />
-                    </div>
-                    <div className="col-md-4">
-                        <Card imgsrc={img2} title="Burger King" />
-                    </div>
-                    <div className="col-md-4">
-                        <Card imgsrc={img3} title="Wendys" />
-                    </div>
-                </div>
+            <div>
+               <Card/>
             </div>
         );
     }
