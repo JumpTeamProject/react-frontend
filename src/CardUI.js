4c706c36187ff677611cf7eda47259cc622bd1c9
crives <car.rives@gmail.com>
2020-09-05 23:04:54 -0400

    up-to-date-code

--- src/CardUI.js
+++ src/CardUI.js
@@ -18,7 +18,7 @@ const Card = props =>{
         <div className="card card-block text-center shadow" onClick={handleClick}>
             <div className="row">
                 <div className="col-lg-3 overflow">
-                    <img src={props.imgsrc} alt='Pizza' className="card-img"/>
+                    <img src={props.imgsrc} alt='Pizza1' className="card-img"/>
                 </div>
                  <div className="col-lg-8 card-body text-dark">
                     <h4 className="card-title">{props.title}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{props.rating}</h4>
