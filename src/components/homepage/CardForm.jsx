68a6566f7464cd7581c873998b89a37ddfcd4d53
crives <car.rives@gmail.com>
2020-09-07 18:49:18 -0400

    Work on add restaurant button

--- src/components/homepage/CardForm.jsx
+++ src/components/homepage/CardForm.jsx
@@ -1,72 +1,63 @@
-// import React, { Component } from 'react';
-// import './form.css';
+import React, { Component } from 'react';
+import './form.css';
 
-// class CardForm extends Component {
+class CardForm extends Component {
 
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             id: props.info.id,
-//             name: props.info.name,
-//             email: props.student.email,
-//             imagePath: props.student.imagePath,
-//             department: props.student.department
-//         };
-//         this.handleChange = this.handleChange.bind(this);
-//         this.handleSubmit = this.handleSubmit.bind(this);
-//     }
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: props.restaurant.id,
+            name: props.restaurant.name,
+            description: props.restaurant.description,
+            imageUrl: props.restaurant.imageUrl,
+            rating: props.restaurant.rating,
+            phoneNumber: props.restaurant.phoneNumber
+       };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-//     handleChange = (event) => {
-//         let nam = event.target.name;
-//         let val = event.target.value;
-//         this.setState({ [nam]: val });
-//     }
+    handleChange = (event) => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({ [nam]: val });
+    }
 
-//     handleSubmit = (event) => {
-//         console.log('submit');
-//         this.props.handleCrud(this.state);
-//     }
+    handleSubmit = (event) => {
+        console.log('submit');
+        this.props.handleCrud(this.state);
+    }
 
-//     render() {
-//         console.log(this.state);
-//         return (
-//             <div>
-//                 <div className="form-style">
-//                     {this.state.id && <h3>{'Changes for ' + this.state.firstName + ' ' + this.state.lastName}</h3>}
-//                     <form onSubmit={this.handleSubmit}>
-//                         <div className='row d-inline-flex'>
-//                             <div className="form-group col">
-//                                 <label >First Name:</label>
-//                                 <input type="text" name="firstName" className="form-control" id="firstName" value={this.state.firstName} onChange={this.handleChange} required />
-//                             </div>
+    render() {
+        console.log(this.state);
+        return (
+            <div>
+                <div className="form-style">
+                    {this.state.id && <h3>{'Changes for ' + this.state.name}</h3>}
+                    <form onSubmit={this.handleSubmit}>
+                        <div className='row d-inline-flex'>
+                            <div className="form-group col">
+                                <label >Restaurant Name:</label>
+                                <input type="text" name="name" className="form-control" id="name" value={this.state.name} onChange={this.handleChange} required />
+                            </div>
+                        </div>
 
-//                             <div className="form-group col">
-//                                 <label >Last Name:</label>
-//                                 <input type="text" name="lastName" className="form-control" id="lastName" value={this.state.lastName} onChange={this.handleChange} required />
-//                             </div>
-//                         </div>
+                        <div className="form-group">
+                            <label >description:</label>
+                            <input type="text" name="description" className="form-control" id="description" value={this.state.description} onChange={this.handleChange} required />
+                        </div>
 
-//                         <div className="form-group">
-//                             <label >email:</label>
-//                             <input type="text" name="email" className="form-control" id="email" value={this.state.email} onChange={this.handleChange} required />
-//                         </div>
+                        <div className="form-group">
+                            <label >Image URL:</label>
+                            <input type="text" name="imageurl" className="form-control" id="imageurl" value={this.state.imageUrl} onChange={this.handleChange} required />
+                        </div>
 
-//                         <div className="form-group">
-//                             <label >Department:</label>
-//                             <input type="text" name="department" className="form-control" id="department" value={this.state.department} onChange={this.handleChange} required />
-//                         </div>
+                        <input type="button" value="Submit" className="btn btn-primary" />
+                    </form>
 
-//                         <div className="form-group">
-//                             <label >Image URL:</label>
-//                             <input type="text" name="imagePath" className="form-control" id="imagePath" value={this.state.imagePath} onChange={this.handleChange} required />
-//                         </div>
-
-//                         <input type="submit" value="Submit" className="btn btn-primary" />
-//                     </form>
-
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-// export default CardForm;
+                </div>
+            </div>
+        );
+    }
+}
+export default CardForm;
--- src/components/homepage/FormModal.js
+++ src/components/homepage/FormModal.js
@@ -1,53 +1,54 @@
-// import React, { useState, useContext } from 'react';
-// import { Button, Modal } from 'react-bootstrap';
-// import StudentForm from './CardForm.jsx';
-// import { createStudent } from '../service/CardService';
+import React, { useState, useContext } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import CardForm from './CardForm.jsx';
+import { createRestaurant } from '../../service/CardService';
+import AddButton from './AddButton';
 
-// const FormModal = (props) => {
-//   const [show, setShow] = useState(false);
-//   const [type, setType] = useState(props.type);
+const FormModal = (props) => {
+  const [show, setShow] = useState(false);
+  const [type, setType] = useState(props.type);
 
-//   const handleClose = () => setShow(false);
-//   const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-//   var student, buttonColor, buttonText, crudFunction;
+  var restaurant, buttonColor, buttonText, crudFunction;
 
-//   // Switch Allows Modal Reuse for Update and Create
-//   switch (type) {
-//     case 'create':
-//       buttonColor = 'primary';
-//       buttonText = type.toString().toUpperCase();
-//       crudFunction = (student) => { createStudent(student); }
-//       student = {};
-//       break;
-//     case 'update':
-//       buttonColor = 'warning';
-//       buttonText = type.toString().toUpperCase();
-//       crudFunction = props.handleUpdate;
-//       student = props.student;
-//       break;
-//     default:
-//   }
+  // Switch Allows Modal Reuse for Update and Create
+  switch (type) {
+    case 'create':
+      buttonColor = 'primary';
+      buttonText = type.toString().toUpperCase();
+      crudFunction = (restaurant) => { createRestaurant(restaurant); }
+      restaurant = {};
+      break;
+    // case 'update':
+    //   buttonColor = 'warning';
+    //   buttonText = type.toString().toUpperCase();
+    //   crudFunction = props.handleUpdate;
+    //   student = props.student;
+    //   break;
+    default:
+  }
 
-//   return (
-//     <>
-//       {<Button variant={buttonColor} onClick={handleShow}>
-//         {buttonText}
-//       </Button>}
+  return (
+    <>
+      {<AddButton variant={buttonColor} onClick={handleShow}>
+        {buttonText}
+      </AddButton>}
 
-//       <Modal show={show} onHide={handleClose} size='lg'>
-//         <Modal.Header closeButton>
-//           <Modal.Title>{buttonText + ' ' + 'Student'}</Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body><StudentForm student={student} handleCrud={crudFunction} /></Modal.Body>
-//         <Modal.Footer>
-//           <Button variant="secondary" onClick={handleClose}>
-//             Close
-//             </Button>
-//         </Modal.Footer>
-//       </Modal>
-//     </>
-//   );
-// }
+      <Modal show={show} onHide={handleClose} size='lg'>
+        <Modal.Header closeButton>
+          <Modal.Title>{buttonText + ' ' + 'Restaurant'}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body><CardForm restaurant={restaurant} handleCrud={crudFunction} /></Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+            </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
 
-// export default FormModal;
+export default FormModal;
