6ebede267b49fc0f687cb1d3a511d563fca9f772
David Morales <davmorales@ucdavis.edu>
2020-09-07 09:52:30 -0700

    Grabbed from Thein

--- src/thien_profile/RestaurantImage.js
+++ src/thien_profile/RestaurantImage.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 const RestaurantImage = (props) => {
     return (
         <div className="overflow" >
-            <img src={props.imgsrc} alt="image" className="card-img-top-column" />
+            <img src={props.imageURL} alt="image" className="card-img-top-column" />
         </div>
     )
 }
